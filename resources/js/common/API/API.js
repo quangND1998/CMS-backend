80e769290dc49ac035f73f2a5b8026541f73ed23
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2021-06-25 11:22:49 +0700

    update_api

--- resources/js/common/API/API.js
+++ resources/js/common/API/API.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import jwtToken from "../token"
 
 const API = axios.create({
-    baseURL: `http://127.0.0.1:8000/api/`,
+    baseURL: `https://cms.nemoviz.com/api/`,
 
 });
 
